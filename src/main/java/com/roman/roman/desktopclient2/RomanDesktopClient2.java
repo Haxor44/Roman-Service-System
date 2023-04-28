f9e6ba4a7a383f82b3f3dc7131241236428be188
Haxor44 <evolmalek04@gmail.com>
2023-04-28 15:06:02 +0300

    first commit

---
+++ src/main/java/com/roman/roman/desktopclient2/RomanDesktopClient2.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.roman.roman.desktopclient2;
+
+/**
+ *
+ * @author hp
+ */
+public class RomanDesktopClient2 {
+
+    public static void main(String[] args) {
+        Login login = new Login();
+        //login();
+        
+    }
+}
